8467fa791aceeb7c1d76e416a419f64f8ae811d5
CripticHood <criptichoodx@gmail.com>
2024-03-27 09:17:21 +0100

    style:UI Initial structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, aut ducimus officiis id hic vitae quaerat earum, cumque nobis reprehenderit perferendis cupiditate mollitia, iure minus rerum natus quia commodi esse!
+    </>
+  )
+}
+
+export default App
